431412f94191d230cf7adbbf3de1b3577f83ba58
kilpy <kilpamations@gmail.com>
2025-12-08 20:47:36 -0500

    Implement fixed timestep loop in fixedtimestep.cpp

---
+++ fixedtimestep.cpp
@@ -0,0 +1,29 @@
+#include <chrono>
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+   float accumulator = 0;
+   float dt = 0.5; // change this according to how long you want each frame to be
+   bool quit = false;
+   std::chrono::steady_clock::time_point current_time = std::chrono::steady_clock::now();
+  
+   size_t current_frame = 0;
+   while (!quit) {
+      std::chrono::steady_clock::time_point new_time = std::chrono::steady_clock::now();
+       float dur_seconds = std::chrono::duration<float>(new_time - current_time).count();
+       
+
+       accumulator += dur_seconds;
+       
+       while (accumulator > dt) {
+           accumulator -= dt;
+           current_frame += 1;
+           
+           printf("%zu \n", current_frame);
+       }
+       current_time = new_time;
+     }
+   
+}
